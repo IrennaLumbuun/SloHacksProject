f8d747ae25d4cde9cabf0417d6bf2f8ac0fb1fd1
PAenugula <paenugul@calpoly.edu>
2020-02-29 23:28:43 -0800

    firebase auth added

---
+++ index.js
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.9.3/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.9.3/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyB1-Y3GEdbUiw-PFMFjkb7L5q530Y417Gw",
+    authDomain: "phaniraj-ce9d7.firebaseapp.com",
+    databaseURL: "https://phaniraj-ce9d7.firebaseio.com",
+    projectId: "phaniraj-ce9d7",
+    storageBucket: "phaniraj-ce9d7.appspot.com",
+    messagingSenderId: "68981850666",
+    appId: "1:68981850666:web:485a4173757c47c8637953",
+    measurementId: "G-9XFCGJ7GSW"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
