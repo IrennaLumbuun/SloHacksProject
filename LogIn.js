a63414375eb13237aad25632b72b1f026e8bddf9
jeremyszeto <jzeto1@gmail.com>
2020-03-01 00:48:38 -0800

    add firebase tools

--- LogIn.js
+++ LogIn.js
@@ -1,3 +1,22 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/database");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB1-Y3GEdbUiw-PFMFjkb7L5q530Y417Gw",
+  authDomain: "phaniraj-ce9d7.firebaseapp.com",
+  databaseURL: "https://phaniraj-ce9d7.firebaseio.com",
+  projectId: "phaniraj-ce9d7",
+  storageBucket: "phaniraj-ce9d7.appspot.com",
+  messagingSenderId: "68981850666",
+  appId: "1:68981850666:web:485a4173757c47c8637953",
+  measurementId: "G-9XFCGJ7GSW"
+};
+
 $(function() {
     $(".btn").click(function() {
       $(".form-signin").toggleClass("form-signin-left");
